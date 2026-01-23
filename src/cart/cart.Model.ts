bbf27484b566498aaa72b93383eeb057e5cb24fc
devHksingh <hksingh.dev@gmail.com>
2026-01-23 16:00:28 +0530

    add cart interface

--- src/cart/cart.Model.ts
+++ src/cart/cart.Model.ts
@@ -1,6 +1,20 @@
 import mongoose from "mongoose";
 
-const cartItemSchema = new mongoose.Schema({
+interface ICartItem {
+  product: mongoose.Types.ObjectId;
+  quantity: number;
+  price: number;
+}
+
+interface ICart extends mongoose.Document {
+  user: mongoose.Types.ObjectId;
+  items: ICartItem[];
+  totalAmount: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const cartItemSchema = new mongoose.Schema<ICartItem>({
   product: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Product",
@@ -18,7 +32,7 @@ const cartItemSchema = new mongoose.Schema({
   },
 });
 
-const cartSchema = new mongoose.Schema(
+const cartSchema = new mongoose.Schema<ICart>(
   {
     user: {
       type: mongoose.Schema.Types.ObjectId,
